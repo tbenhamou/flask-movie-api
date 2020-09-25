44e39ad69d93a9533e9226ee2f4bedbff104f32a
Thomas <tbenhamou@gmail.com>
2020-09-25 14:51:09 +0300

    add: blueprint movies resource

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from database.db import initialize_db
+from resources.movie import movies
+
+app = Flask(__name__)
+
+app.config['MONGODB_SETTINGS'] = {
+ 'host': 'mongodb://localhost/movie-bag'
+}
+
+initialize_db(app)
+app.register_blueprint(movies)
+
+app.run()
